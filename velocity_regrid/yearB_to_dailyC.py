044ff84ac21563133eb13e3fd4604a28ad0cd583
nathanieltarshish <nathaniel.tarshish@noaa.gov>
2016-11-15 12:03:01 -0500

    changed to script form

--- velocity_regrid/yearB_to_dailyC.py
+++ velocity_regrid/yearB_to_dailyC.py
@@ -3,17 +3,43 @@ import numpy as np
 import numpy.ma as ma
 import sys
 import os
+import argparse
 
-storagepath = '/archive/net/CM2.6/regrid/'
-out_folder = '/archive/net/CM2.6/regrid/02020101/'
 
-half_len_data = nc.Dataset(storagepath+'CM2p6_half_len.nc')
+parser = argparse.ArgumentParser(
+        description='generate daily surface C grid velocites files from a years worth of B grid daily velocities ')
+
+parser.add_argument('--C_grid_spec', metavar='C_grid_spec', default='/archive/net/CM2.6/regrid/CM2p6_half_len.nc',
+                    help='the netcdf file containing the C grid half-face lengths')
+
+parser.add_argument('--B_vel', type=str, metavar='B_vel',
+                    help='netcdf file containing a years worth of B grid daily surface velocities')
+
+parser.add_argument('--out', type=str, metavar='out', 
+                    help='the output folder for C grid daily surface velocity files')
+
+args = parser.parse_args()
+
+# storagepath = '/archive/net/CM2.6/regrid/'
+# out_folder = '/archive/net/CM2.6/regrid/02020101/'
+
+half_len_data = nc.Dataset(args.C_grid_spec)
 lat_T = half_len_data.variables['grid_y_T'][0:2107]
 lon_T = half_len_data.variables['grid_x_T'][...]
+du_Eface_N = half_len_data.variables['ds_21_22_T'][0:2107, :] #omits polar region
+du_Eface_S = half_len_data.variables['ds_20_21_T'][0:2107, :] #omits polar region 
+du_Eface_N = np.transpose(du_Eface_N) 
+du_Eface_S = np.transpose(du_Eface_S) 
+half_len_data.close()
 
-vel_sample = nc.Dataset(storagepath+'surf_vel_sample.nc')
-lat_U = vel_sample.variables['yu_ocean'][0:2107]
-lon_U = vel_sample.variables['xu_ocean'][...]
+
+# path2file = '/archive/Richard.Slater/CM2.6/CM2.6_A_Control-1860_V03/history/'
+# filename = '02020101.ocean_minibling_surf_field.nc'
+
+vel_data = nc.Dataset(args.B_vel)
+lat_U = vel_data.variables['yu_ocean'][0:2107]
+lon_U = vel_data.variables['xu_ocean'][...]
+vel_data.close()
 
 xu_c = np.zeros(np.shape(lon_U))
 yu_c = np.zeros(np.shape(lat_U))
@@ -37,10 +63,7 @@ for j in range(1,len(lat_U)):
 yv_c[0] = lat_U[0] + (lat_U[0] - lat_U[1])
 
 #the regrid loop 
-path2file = '/archive/Richard.Slater/CM2.6/CM2.6_A_Control-1860_V03/history/'
-filename = '02020101.ocean_minibling_surf_field.nc'
-path = path2file+filename
-vel_data = nc.Dataset(path)
+vel_data = nc.Dataset(args.B_vel)
 time = vel_data.variables['time'][:]
 
 for tind in range(len(time)):
@@ -52,12 +75,6 @@ for tind in range(len(time)):
 
 	u_B = ma.filled(u_B, fill_value=0.0) #change fill value from -1e20 to 0.0 
 	v_B = ma.filled(v_B, fill_value=0.0) #change fill value from -1e20 to 0.0 
-
-	du_Eface_N = half_len_data.variables['ds_21_22_T'][0:2107, :] #omits polar region
-	du_Eface_S = half_len_data.variables['ds_20_21_T'][0:2107, :] #omits polar region 
-
-	du_Eface_N = np.transpose(du_Eface_N) 
-	du_Eface_S = np.transpose(du_Eface_S) 
 
 	u_C = np.zeros(np.shape(u_B))
 	v_C = np.zeros(np.shape(v_B))
@@ -74,10 +91,10 @@ for tind in range(len(time)):
 	        u_C[i,j] = ( (du_Eface_N[i-1, j]*u_B[i-1, j] + du_Eface_S[i-1, j]*u_B[i-1, j-1]) \
 	                    /(du_Eface_N[i, j-1] + du_Eface_S[i, j-1] ) )  
 
-	out_filename_u = filename[0:7]+'_'+str(time[tind])+'_'+'U.nc'
-	print 'Saving reformat to: '+out_folder+out_filename_u
+	out_file_u = args.out+'U'+str(tind).zfill(10)+'.nc'
+	print 'Saving reformat to: '+out_file_u
 
-	netcdf_file = nc.Dataset(out_folder+out_filename_u, 'w', format='NETCDF4')
+	netcdf_file = nc.Dataset(out_file_u, 'w', format='NETCDF4')
 	netcdf_file.createDimension('time', 1)
 	netcdf_file.createDimension('yu_c', len(yu_c))
 	netcdf_file.createDimension('xu_c', len(xu_c))
@@ -107,10 +124,10 @@ for tind in range(len(time)):
 
 	netcdf_file.close()
 
-	out_filename_v = filename[0:7]+'_'+str(time[tind])+'_'+'V.nc'
-	print 'Saving reformat to: '+out_folder+out_filename_v
+	out_filename_v = args.out+'V'+str(tind).zfill(10)+'.nc'
+	print 'Saving reformat to: '+out_filename_v
 
-	netcdf_file = nc.Dataset(out_folder+out_filename_v, 'w', format='NETCDF4')
+	netcdf_file = nc.Dataset(out_filename_v, 'w', format='NETCDF4')
 	netcdf_file.createDimension('time', 1)
 	netcdf_file.createDimension('yv_c', len(yv_c))
 	netcdf_file.createDimension('xv_c', len(xv_c))
